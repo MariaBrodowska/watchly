efadc176906213c437de8e9ddbc71a36e0aeb4f2
Maria <mariabrodowska696@gmail.com>
2025-02-06 00:45:37 +0100

    fetching and displaying data from TMDb API

--- app/src/main/java/com/example/watchly/MainActivity.java
+++ app/src/main/java/com/example/watchly/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.watchly;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -9,12 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public class MainActivity extends AppCompatActivity {
     interface RequestDiscover{
-        @GET("https://api.themoviedb.org/3/discover/movie?api_key={apiKey}")
-
+        @GET("discover/movie")
+        Call<MovieResponse> getMovies(@Query("api_key") String apiKey);
     }
     TextView textView;
     @Override
@@ -28,6 +38,33 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         textView = findViewById(R.id.result);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/3/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        RequestDiscover requestDiscover = retrofit.create(RequestDiscover.class);
+        String apiKey = BuildConfig.TMDB_API_KEY;
+        requestDiscover.getMovies(apiKey).enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                if (response.isSuccessful()) {
+                    List<Movie> movies = response.body().getResults();
+                    String titleList = "";
+                    for(Movie movie : movies) titleList += movie.getTitle() + "\n";
+                    titleList += String.valueOf(movies.size());
+//                    textView.setText(response.body().getResults().get(3).getTitle());
+                    textView.setText(titleList);
+                } else {
+                    textView.setText("Błąd: " + response.code() + ", " + response.message());
+                    Log.e("API Error", "Error body: " + response.errorBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable throwable) {
+                textView.setText("Błąd połączenia: " + throwable.getMessage());
+            }
+        });
     }
 }
