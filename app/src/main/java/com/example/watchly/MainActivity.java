fbf3c13f165b7b1f8d6db6ed3a170625491e2452
Maria <mariabrodowska696@gmail.com>
2025-02-05 23:40:15 +0100

    init commit

---
+++ app/src/main/java/com/example/watchly/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.watchly;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import retrofit2.http.GET;
+
+public class MainActivity extends AppCompatActivity {
+    interface RequestDiscover{
+        @GET("https://api.themoviedb.org/3/discover/movie?api_key={apiKey}")
+
+    }
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        textView = findViewById(R.id.result);
+
+    }
+}
---
+++ app/src/main/java/com/example/watchly/MovieResponse.java
@@ -0,0 +1,24 @@
+package com.example.watchly;
+
+import java.util.List;
+
+public class MovieResponse {
+    private int page;
+    private List<Movie> results;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Movie> results) {
+        this.results = results;
+    }
+}
